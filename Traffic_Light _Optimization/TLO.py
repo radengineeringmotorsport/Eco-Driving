7503156721e0c4d3ca6004f91812ffc06931acba
radengineeringmotorsport <66329460+radengineeringmotorsport@users.noreply.github.com>
2020-07-15 14:40:52 -0400

    Add files via upload

--- Traffic_Light _Optimization/TLO.py
+++ Traffic_Light _Optimization/TLO.py
@@ -1,1 +1,15 @@
-''' Hello ''
+''' Hello '''
+
+def cost_equals_l2cost (k, a, b);
+global toll
+#compute l2 norm squared of the second derivative of the polynomial 
+# f=k(1)-(x-a)^3+k(2)*(x-a)^2+k(3)-(x-a)+k(4)
+# cost=int_a^b (f''(t))^2 dt
+
+if abs(k(1))<toll*1e-3,
+	cost=4*k(2)^2*(b-a);
+
+elif cost=1/18/k(1)*((6*k(1)*(b-a)+2*k(2))^3-(2*k(2))^3);
+
+return;
+
